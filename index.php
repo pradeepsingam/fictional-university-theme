499cc80c20bfda9c2e2e807a9e27296ce382f6e5
pradeep <ipradeep.here@gmail.com>
2024-12-27 10:47:06 +0530

    single post with post loops

--- index.php
+++ index.php
@@ -19,3 +19,15 @@ greet("prad", "orange")
     $count++;
  }
 ?>
+
+<h1> Recent Blogs </h1>
+
+<?php
+while(have_posts()){
+    the_post(); ?>
+    <h2> <a href="<?php the_permalink();?>"> <?php the_title(); ?> </a></h2>
+    
+    <hr>
+<?php
+}
+?>
